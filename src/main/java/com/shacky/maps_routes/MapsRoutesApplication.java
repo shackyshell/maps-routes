6215eb3719ecd61c73ca15925a68b40d580733a0
Julia Zajusz <17937394+shackyshell@users.noreply.github.com>
2024-09-05 14:25:08 +0200

    spring initializr

---
+++ src/main/java/com/shacky/maps_routes/MapsRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.shacky.maps_routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapsRoutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shacky/maps_routes/MapsRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shacky.maps_routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
